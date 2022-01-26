8ba7ab9fa59d37de9aa785d005525a91bd164aaa
cornel <ghita.cornel@gmail.com>
2022-01-26 23:13:48 +0200

    better

--- src/test/java/tests/mappers/orika/TestOrika.java
+++ src/test/java/tests/mappers/orika/TestOrika.java
@@ -8,7 +8,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import tests.common.Utils;
 
-public class TestOrikaMapper {
+public class TestOrika {
 
     OrikaMapper mapper = new OrikaMapper();
 
