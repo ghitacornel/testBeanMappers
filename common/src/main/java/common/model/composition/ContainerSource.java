0ab2e104cf0a9babeb5f47b1c86d082e814334d0
cornel <ghita.cornel@gmail.com>
2022-02-11 18:38:01 +0200

    better

--- common/src/main/java/common/model/composition/ContainerSource.java
+++ common/src/main/java/common/model/composition/ContainerSource.java
@@ -7,9 +7,16 @@ public class ContainerSource {
 
     private String containerDetail;
 
+    private ItemSource singleItem;
+
     public static ContainerSource input() {
+
+        ItemSource itemSource = new ItemSource();
+        itemSource.setItemDetail("item detail");
+
         ContainerSource model = new ContainerSource();
         model.setContainerDetail("container detail");
+        model.setSingleItem(itemSource);
         return model;
     }
 }
--- common/src/main/java/common/model/composition/ContainerTarget.java
+++ common/src/main/java/common/model/composition/ContainerTarget.java
@@ -7,12 +7,19 @@ public class ContainerTarget {
 
     private String containerDetail;
 
+    private ItemTarget singleItem;
+
     /**
      * @return expected mapped value
      */
     public static ContainerTarget expected() {
+
+        ItemTarget itemTarget = new ItemTarget();
+        itemTarget.setItemDetail("item detail");
+
         ContainerTarget model = new ContainerTarget();
         model.setContainerDetail("container detail");
+        model.setSingleItem(itemTarget);
         return model;
     }
 }
