b65db0fe23cf1e4147c199baee4dead9d7db4c9c
cornel <ghita.cornel@gmail.com>
2022-02-11 19:01:43 +0200

    better

--- common/src/main/java/common/model/composition/ContainerSource.java
+++ common/src/main/java/common/model/composition/ContainerSource.java
@@ -2,21 +2,41 @@ package common.model.composition;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class ContainerSource {
 
     private String containerDetail;
 
     private ItemSource singleItem;
+    private List<ItemSource> multipleItems;
 
     public static ContainerSource input() {
 
-        ItemSource itemSource = new ItemSource();
-        itemSource.setItemDetail("item detail");
-
         ContainerSource model = new ContainerSource();
         model.setContainerDetail("container detail");
-        model.setSingleItem(itemSource);
+
+        {
+            ItemSource itemSource = new ItemSource();
+            itemSource.setItemDetail("item detail");
+            model.setSingleItem(itemSource);
+        }
+
+        {
+            ItemSource multipleItems1 = new ItemSource();
+            multipleItems1.setItemDetail("item detail 1");
+            ItemSource multipleItems2 = new ItemSource();
+            multipleItems2.setItemDetail("item detail 2");
+            ItemSource multipleItems3 = new ItemSource();
+            multipleItems3.setItemDetail("item detail 3");
+            model.setMultipleItems(new ArrayList<>());
+            model.getMultipleItems().add(multipleItems1);
+            model.getMultipleItems().add(multipleItems2);
+            model.getMultipleItems().add(multipleItems3);
+        }
+
         return model;
     }
 }
--- common/src/main/java/common/model/composition/ContainerTarget.java
+++ common/src/main/java/common/model/composition/ContainerTarget.java
@@ -2,24 +2,43 @@ package common.model.composition;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class ContainerTarget {
 
     private String containerDetail;
 
     private ItemTarget singleItem;
+    private List<ItemTarget> multipleItems;
 
     /**
      * @return expected mapped value
      */
     public static ContainerTarget expected() {
 
-        ItemTarget itemTarget = new ItemTarget();
-        itemTarget.setItemDetail("item detail");
-
         ContainerTarget model = new ContainerTarget();
         model.setContainerDetail("container detail");
-        model.setSingleItem(itemTarget);
+
+        {
+            ItemTarget itemTarget = new ItemTarget();
+            itemTarget.setItemDetail("item detail");
+            model.setSingleItem(itemTarget);
+        }
+
+        {
+            ItemTarget multipleItems1 = new ItemTarget();
+            multipleItems1.setItemDetail("item detail 1");
+            ItemTarget multipleItems2 = new ItemTarget();
+            multipleItems2.setItemDetail("item detail 2");
+            ItemTarget multipleItems3 = new ItemTarget();
+            multipleItems3.setItemDetail("item detail 3");
+            model.setMultipleItems(new ArrayList<>());
+            model.getMultipleItems().add(multipleItems1);
+            model.getMultipleItems().add(multipleItems2);
+            model.getMultipleItems().add(multipleItems3);
+        }
         return model;
     }
 }
